00e2af3d2cf070fa9196ec415a670533089649eb
Admin <admin@iMac-2.local>
2021-11-16 17:28:12 +0000

    added translation settings

--- wordpress/wp-content/plugins/appsmo_gallery/views/appsmo-gallery-admin-display.php
+++ wordpress/wp-content/plugins/appsmo_gallery/views/appsmo-gallery-admin-display.php
@@ -7,32 +7,11 @@
     <?php do_settings_sections( 'appsmo-gallery-settings-group' ); ?>
     <table class="form-table">
         <tr valign="top">
-        <th scope="row">Add Category</th>
+        <th scope="row"><?php echo __("Add Category", $this->plugin_title); ?> </th>
         <td><input type="text" name="appsmo_gallery_category_dropdown_settings" /></td>
-        </tr>
-
-		<?php $options = get_option( 'appsmo_gallery_category_dropdown_settings' ); ?>
-		<tr valign="top">
-        <th scope="row">All categories</th>
-        <td>
-			<select name='dropdown_settings[select_field_0]'>
-				<option value='animals' <?php selected( $options['appsmo_gallery_category_dropdown_0'], 1 ); ?>>Animals</option>
-				<option value='fashion' <?php selected( $options['appsmo_gallery_category_dropdown_0'], 2 ); ?>>Fashion</option>
-				<option value='film' <?php selected( $options['appsmo_gallery_category_dropdown_0'], 4 ); ?>>Film</option>
-				<option value='people' <?php selected( $options['appsmo_gallery_category_dropdown_0'], 5 ); ?>>People</option>
-				<option value='health' <?php selected( $options['appsmo_gallery_category_dropdown_0'], 6 ); ?>>Health & Wellness</option>
-				<option value='travel' <?php selected( $options['appsmo_gallery_category_dropdown_0'], 7 ); ?>>Travel</option>
-				<option value='nature' <?php selected( $options['appsmo_gallery_category_dropdown_0'], 8 ); ?>>Nature</option>
-				<option value='architecture' <?php selected( $options['appsmo_gallery_category_dropdown_0'], 9 ); ?>>Architecture</option>
-				<option value='foodanddrink' <?php selected( $options['appsmo_gallery_category_dropdown_0'], 10 ); ?>>Food & Drink</option>
-			</select>
-		</td>
-        </tr>
-		
-
-         
+        </tr> 
         <tr valign="top">
-        <th scope="row">Image count</th>
+        <th scope="row"><?php echo __("Image count", $this->plugin_title); ?></th>
         <td><input type="number" id="image_count" name="appsmo_gallery_count" value="<?php echo esc_attr( get_option('appsmo_gallery_count') ); ?>" /></td>
         </tr>
         
